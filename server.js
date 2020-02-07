8ddcbf21d9daf75da833b7cb7c386f1075d078c6
Nick Oelsner <noelsner@gmail.com>
2020-02-06 21:14:16 -0800

    styling

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-const readJSON = require('./db')
+const readJSON = require('./db');
 
 console.log(readJSON)
 
 //const prods =  .then(ret => console.log(ret))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-}); 
+});
 
 app.get('/api/products', (req, res, next) => {
     readJSON.readJSON('./products.json')
     .then( prods => res.send(prods))
-}); 
+    .catch(ex => next(ex))
+});
 
 app.listen(3002, () => {console.log('listening thru port 3002')})
