75f7f2c27bea90af4f26cfbe2defa9bd602f6ab9
XtremePineapple <95henryX@gmail.com>
2020-02-06 20:16:49 -0800

    phayze one

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('hello world')
-});
+const readJSON = require('./db')
 
-app.listen(3000, () => {console.log('listening thru port 3000')})
+console.log(readJSON)
+
+//const prods =  .then(ret => console.log(ret))
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+}); 
+
+app.get('/api/products', (req, res, next) => {
+    readJSON.readJSON('./products.json')
+    .then( prods => res.send(prods))
+}); 
+
+app.listen(3002, () => {console.log('listening thru port 3002')})
